41981cb5e33368beade4f661b7aaba83235eb428
themirick <themirick.@gmail.com>
2023-03-01 13:48:01 +0500

    Add solution of fina project to the console programm

--- console_program/Program.cs
+++ console_program/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Выборка элементов из данного массива в новый массив по условию
+// Будут приведены несколько способов решения
+
+
+string[] stringArray = new string[] 
+    {
+    ":-)", "hello", "2", "world", "-2", 
+    "Russia", "Kazan", "123456", "computer science",
+    "mother", "GeekBrains", "GB", "car" 
+    };
+
+// 1st method
+
+string[] SortArrayByLength(string[] stringArray, int sortingElementLength)
+{
+    string tempString = String.Empty;
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        if (stringArray[i].Length <= sortingElementLength) 
+            tempString = string.Concat(tempString, stringArray[i], " ");
+    }
+
+    string[] sortedArray = tempString.Split(" ");
+    return sortedArray;
+}
+
+
+
+string[] s = SortArrayByLength(stringArray, 3);
+
+foreach (string i in s)
+{
+    System.Console.WriteLine(i);
+}
+
+
+// 2st method with Linq operators
+
+// var selectElements = stringArray.Where(i => i.Length <= sortingElementLength);
+
+// foreach(string i in selectElements)
+//     System.Console.WriteLine(i);
+
